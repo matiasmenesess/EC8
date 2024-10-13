ddb9f9f9a06f9ac09ba2999d8d32c8fe30768cfa
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-12 20:37:06 -0500

    Update problema_4.cpp

--- problema_4.cpp
+++ problema_4.cpp
@@ -2,15 +2,16 @@
 //1. Olivares Sarmiento, Javier Ignacio Leon
 //2. Silva Reyes, Santiago Miguel
 //3. Meneses Roncal, Matias Alonso
+
 #include <vector>
 #include <iostream>
 using namespace std;
+
 struct Elemento {
-    int value;
+    int val;
     int fila;
     int columna;
 };
-
 
 class Heap {
     vector<Elemento> heap;
@@ -18,10 +19,10 @@ class Heap {
         int smallest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        if (left < heap.size() && heap[left].value < heap[smallest].value) {
+        if (left < heap.size() && heap[left].val < heap[smallest].val) {
             smallest = left;
         }
-        if (right < heap.size() && heap[right].value < heap[smallest].value) {
+        if (right < heap.size() && heap[right].val < heap[smallest].val) {
             smallest = right;
         }
         if (smallest != i) {
@@ -29,21 +30,24 @@ class Heap {
             heapify_down(smallest);
         }
     }
+
     void heapify_up(int i) {
         int parent = (i - 1) / 2;
-        if (i && heap[i].value < heap[parent].value) {
+        if (i && heap[i].val < heap[parent].val) {
             swap(heap[i], heap[parent]);
             heapify_up(parent);
         }
     }
+
 public:
     void insert(Elemento node) {
         heap.push_back(node);
         heapify_up(heap.size() - 1);
     }
+
     Elemento top() {
         if (heap.size() == 0) {
-            cout<<"Heap vacio";
+//            cout << "Heap vacio" << endl;
         }
         Elemento raiz = heap[0];
         heap[0] = heap.back();
@@ -52,28 +56,37 @@ public:
 
         return raiz;
     }
+
     bool empty() const {
         return heap.size() == 0;
     }
 };
+
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         Heap heap;
         for (int i = 0; i < matrix.size(); i++) {
             Elemento el;
-            el.value = matrix[i][0];
-            el.fila=i;
-            el.columna=0;
+            el.val = matrix[i][0];
+            el.fila = i;
+            el.columna = 0;
             heap.insert(el);
         }
+
         Elemento element;
         for (int i = 0; i < k; i++) {
             element = heap.top();
             if (element.columna + 1 < matrix.size()) {
-                heap.insert({matrix[element.fila][element.columna + 1], element.fila, element.columna + 1});
+                Elemento nuevoElemento;
+                nuevoElemento.val = matrix[element.fila][element.columna + 1];
+                nuevoElemento.fila = element.fila;
+                nuevoElemento.columna = element.columna + 1;
+                heap.insert(nuevoElemento);
             }
         }
-        return element.value;
+
+        return element.val;
     }
 };
+
